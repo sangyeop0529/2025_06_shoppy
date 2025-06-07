f56a9b144b75595e13d695e0b2fc9ac0dee39160
DELL <sangyeop0529@gmail.com>
2025-06-07 23:59:07 +0900

    router 설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFound from "./pages/NotFound.jsx";
+import ProductDetail from "./pages/ProductDetail.jsx";
+import Home from "./pages/Home.jsx";
+import AllProducts from "./pages/AllProducts.jsx";
+import NewProduct from "./pages/NewProduct.jsx";
+import MyCart from "./pages/MyCart.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, path: "/", element: <Home /> },
+      { path: "/products", element: <AllProducts /> },
+      { path: "/products/new", element: <NewProduct /> },
+      { path: "/products/:id", element: <ProductDetail /> },
+      { path: "/carts", element: <MyCart /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
