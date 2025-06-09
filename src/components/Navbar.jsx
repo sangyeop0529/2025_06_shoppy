2942404da1f5636568ec044876dbe1d32c300a59
DELL <sangyeop0529@gmail.com>
2025-06-09 14:04:48 +0900

    protect 생성

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { FiShoppingBag } from "react-icons/fi";
 import { BsFillPencilFill } from "react-icons/bs";
-import { login, logout, onUserStateChange } from "../api/firebase";
 import User from "./User";
 import Button from "./ui/Button";
+import { useAuthContext } from "./context/AuthContext";
 
 const Navbar = () => {
-  const [user, setUser] = useState();
-  useEffect(() => {
-    onUserStateChange((user) => {
-      console.log(user);
-      setUser(user);
-    });
-  }, []);
-
+  const { user, login, logout } = useAuthContext();
   return (
     <header className="flex justify-between border-b border-gray-300 p-2">
       <Link to="/" className="flex items-center text-4xl text-brand">
@@ -23,7 +16,7 @@ const Navbar = () => {
       </Link>
       <nav className="flex items-center shrink-0 gap-4 font-semibold">
         <Link to="/products">Products</Link>
-        <Link to="/carts">Carts</Link>
+        {user && <Link to="/carts">Carts</Link>}
         {user && user.isAdmin && (
           <Link to="/products/new" className="text-2xl">
             <BsFillPencilFill />
