8be1f6837b6d4619a0edb9abecb3c8b710f58209
DELL <sangyeop0529@gmail.com>
2025-06-09 13:49:31 +0900

    login styling

---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Button = ({ text, onClick }) => {
+  return (
+    <button
+      className="bg-brand text-white py-2 px-4 rounded-sm hover:brightness-110"
+      onClick={onClick}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
